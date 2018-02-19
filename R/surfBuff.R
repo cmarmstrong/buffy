e40fe0a00960a2262fb07219d77f2a3f54a770fd
cmarmstrong <omni.armstrong@gmail.com>
2018-02-19 07:44:52 -0600

    finish debug newBuff, began last step

--- R/surfBuff.R
+++ R/surfBuff.R
@@ -33,34 +33,11 @@
 #' x <- sf::st_transform(x, 3083)
 #' p <- sf::st_transform(p, 3083)
 #' x <- sf::st_sf(sf::st_cast(x, 'POINT'))
-#' ## TODO union p and average s values; overlapping polygons may cause a problem
+#' ## TODO: handle overlapping polygons
 #' surfBuff(x, p, 3e3*m)
-#' 
-#' data(urbanUS)
-#' x <- c(-92.44744828628, 34.566107548536)
-#' sp::proj4string(aabb) <- sp::CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
-#' query <- osmdata::opq(sp::bbox(aabb))
-#' query <- osmdata::add_osm_feature(query, 'shop', 'supermarket', value_exact=FALSE)
-#' osmSupermarkets <- osmdata::osmdata_sf(query) # NOTICE switch to sf
-#' sfSupermarkets <- osmSupermarkets $osm_points
-#' sfSupermarkets <- sf::st_transform(sfSupermarkets, 3083) # Albers EAC for buffering
-#' forCrop <- sf::st_buffer(sfSupermarkets, 11*mi)
-#' rCropped <- raster::crop(urbanUS, as(forCrop, 'Spatial'))
-#' p <- raster::rasterToPolygons(rCropped, digits=7, dissolve=TRUE)
-#' p <- sf::st_as_sf(p)
-#' x <- sf::st_sf(geometry=sf::st_sfc(sf::st_point(x)))
-#' sf::st_crs(x) <- 4326
-#' x <- sf::st_transform(x, 3083)
-#' s <- list(urbanUS=c('1'=10))
-#' d <- 10*mi
-#' units(d) <- m
-#' foodDeserts <- surfBuff(sfSupermarkets, p, s, d)
-#' plot(rCropped, col='orange', legend=FALSE)
-#' plot(sf::st_geometry(p), add=TRUE)
-#' plot(sf::st_geometry(foodDeserts), add=TRUE)
 #' @export
 surfBuff <- function(x, p, d) {
-    ## browser()
+    browser()
     if(sf::st_crs(x)!=sf::st_crs(p)) stop('CRS of x and p do not match')
     else crsBuf <- sf::st_crs(x)
     buf <- sf::st_buffer(x, d)
@@ -79,23 +56,21 @@ surfBuff <- function(x, p, d) {
     })
     sfLs <- do.call(rbind, lSf)
     sf::st_crs(sfLs) <- crsBuf
+    ## intersect linestrings with p and ensure order matches
     sfIn <- sf::st_intersection(sfLs, p)
+    sfLsIn <- merge(sfLs, sfIn[, c('idLs', 'idFeature') ,drop=TRUE], by=c('idLs', 'idFeature'))
+    sfLsIn <- sfLsIn[with(sfLsIn, order(idFeature, idLs)), ]
+    sfIn <- sfIn[with(sfIn, order(idFeature, idLs)), ]
     newBuffer <- mapply(function(lstring, mls) { # attenuate linestrings by surface effects
-        browser()
         mls <- sf::st_cast(mls, 'MULTILINESTRING')
-        coordsLs <- sf::st_coordinates(lstring) # start and end point
+        coordsLs <- sf::st_coordinates(lstring) # start and end points
         coordsLs[, 'L1'] <- c(0, 0)
         coordsMls <- sf::st_coordinates(mls)
         coordsLs <- cbind(coordsLs, L2=c(coordsMls[1, 'L2'], coordsMls[nrow(coordsMls), 'L2']))
         coordsMls <- rbind(coordsLs[1, ], coordsMls, coordsLs[2, ])
-        sfLs <- lapply(unique(coordsMls[, 'L2']), function(L2) {
-            ## browser()
+        sfLs <- lapply(unique(coordsMls[, 'L2']), function(L2) { # build linestrings and effects
             coordsSub <- coordsMls[coordsMls[, 'L2']==L2, c('X', 'Y')]
             s <- c(rep(c(1, mls[L2, ] $s), (nrow(coordsSub)-1)%/%2), 1)
-            ## s <- sapply(names(s), function(name) { # surface types
-            ##     ## NOTE insert distance factors, instead of identifiers?  see note in 'scaling'
-            ##     c(rep(c(0, mls[L2, name, drop=TRUE]), (nrow(coordsSub)-1)%/%2), 0)
-            ## })
             geom <- lapply(nrow(coordsSub):2, function(i) {
                 sf::st_linestring(rbind(coordsSub[i, ], coordsSub[i-1, ]))
             })
@@ -104,31 +79,24 @@ surfBuff <- function(x, p, d) {
         sfLs <- do.call(rbind, sfLs)
         sf::st_crs(sfLs) <- crsBuf
         lenLs <- sf::st_length(sfLs)
-        lenLs <- lenLs * sfLs $s
-        ## lenLs <- sapply(names(s), function(name) { # scaling by surface types
-        ##     ## NOTE see note in 'surface types'
-        ##     isSurf <- sfLs[, name, drop=TRUE]==1
-        ##     lenLs[isSurf] <- lenLs[isSurf] * s[[name]]
-        ##     lenLs
-        ## })
+        lenLs <- lenLs * sfLs $s # scale lengths by surface effects
         csumLs <- cumsum(lenLs)
         units(csumLs) <- units(d)
-        xsLs <- csumLs > d
-        lsXs <- sfLs[xsLs, ][1, ]
-        lenXs <- csumLs[xsLs][1] - d
-        ## here
-        iStart <- rev(which(!xsLs))[1]
-        ## lenStart <- rev(csumLs[!xsLs])[1]
-        lenStart <- csumLs[iStart]
-        lenOk <- d - lenStart
+        xsLs <- csumLs > d # the linestrings exceeding d
+        lsXs <- sfLs[xsLs, ][1, ] # the xs linestring
+        lenXs <- csumLs[xsLs][1] - d # the xs length
+        iStart <- rev(which(!xsLs))[1] # index of last ok linestring
+        lenStart <- csumLs[iStart] # total length of ok linestrings
+        lenOk <- d - lenStart # ok length of xs linestring
+        lenOk <- lenOk / sfLs $s[iStart+1] # descale length by surface effect
         xsCoords <- sf::st_coordinates(sf::st_transform(lsXs, 4326))[, c('X', 'Y')]
         b <- geosphere::bearing(xsCoords)
-        ## descale lenOk: sfLs $s[iStart] ?
         newPnt <- geosphere::destPoint(xsCoords, b, lenOk)[1, ]
         sfcPnt <- sf::st_sfc(sf::st_point(newPnt))
         sf::st_crs(sfcPnt) <- 4326
         sf::st_coordinates(sf::st_transform(sfcPnt, crsBuf))
-    }, split(sfLs, 1:nrow(sfLs)), split(sfIn, 1:nrow(sfIn)))
+    }, split(sfLsIn, 1:nrow(sfLsIn)), split(sfIn, 1:nrow(sfIn)))
+    ## here. need to replace intersected lines from sfLs with those in newBuffer
     newBuffer <- t(newBuffer)
     lNewBuffer <- lapply(split(newBuffer, rep(1:29, each=121)), matrix, ncol=2)
     sfcNewBuffers <- sf::st_sfc(lapply(lNewBuffer, function(newBuffer) {
